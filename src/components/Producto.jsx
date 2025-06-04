cdfb4a3ecdb0cd2a810544b9343b2a72bb691dd7
Agus Aguero <agus_aguero99@hotmail.com>
2025-06-03 21:05:14 -0300

    ultimos cambios

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,19 +1,21 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-function Producto({nombre,descripcion,marca,precio,id,categorias,imagen}) {
+function Producto({ nombre, descripcion, marca, precio, id, categorias, imagen }) {
+  const producto = { nombre, descripcion, marca, precio, id, categorias, imagen };
+
   return (
-  <div className="card">
-    <Link to={`/productos/${id}`} className="card">
+    <div className="card">
+      <Link to={`/productos/${id}`} className="card">
         <img src={imagen} alt={nombre} className="producto-imagen" />
         <h2>{nombre}</h2>
         <h2>${precio}</h2>
         <p>{marca}</p>
-    </Link>
-          <button onClick={() => agregarAlCarrito(producto)} className="btn-agregar">
-        Agregar al carrito
+      </Link>
+      <button onClick={() => agregarAlCarrito(producto)} className="btn-agregar">
+      Agregar al carrito
       </button>
     </div>
-  )
+  );
 }
 
-export default Producto
+export default Producto;
